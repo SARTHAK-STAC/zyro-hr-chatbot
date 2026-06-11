7c57ed70b224be891a0167576e7709209015b30a
sarthak singh chauhan <scchessking@gmail.com>
2026-06-11 20:25:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
 import streamlit as st
+os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "zyro-rag-challenge"
 
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
